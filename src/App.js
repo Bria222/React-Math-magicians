9f5aa80a353cb6543fe49b50af087ca42586222e
Bria222 <briantechologies.co.ke@gmail.com>
2022-05-26 16:58:47 +0300

    fix linters error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
